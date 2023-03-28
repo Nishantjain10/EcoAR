742b0fbfecbaa92df0470a77ac1608fe1a0bd63a
Nishantjain10 <nishant.jain.775@gmail.com>
2023-03-28 14:53:27 +0530

    project initial commit

---
+++ ecoar/lib/model/homelist.dart
@@ -0,0 +1,44 @@
+import 'package:best_flutter_ui_templates/design_course/home_design_course.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
+import 'package:best_flutter_ui_templates/hotel_booking/hotel_home_screen.dart';
+import 'package:best_flutter_ui_templates/introduction_animation/introduction_animation_screen.dart';
+import 'package:best_flutter_ui_templates/login/pages/login_page.dart';
+import 'package:best_flutter_ui_templates/login/pages/signup_page.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+      imagePath: 'assets/introduction_animation/introduction_animation.png',
+      navigateScreen: IntroductionAnimationScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/hotel/hotel_booking.png',
+      navigateScreen: HotelHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/design_course.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/login_page_image.png',
+      navigateScreen: LoginPage(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/signup_page_image.png',
+      navigateScreen: SignupPage(),
+    ),
+  ];
+}
